4be795dc888a9e1dc1fdfda9988ba8eefc31e058
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-05-06 11:42:02 +0530

    Program for 2d array

--- 2d-array.java
+++ 2d-array.java
@@ -20,8 +20,17 @@ class Result {
      */
 
     public static int hourglassSum(List<List<Integer>> arr) {
-    // Write your code here
-        return 0;
+        
+        int ans = Integer.MIN_VALUE;
+        int n = 6;
+        for(int i=0;i<n-2;i++){
+            for(int j=0;j<n-2;j++){
+                int sum = arr.get(i).get(j) + arr.get(i).get(j+1) + arr.get(i).get(j+2) + arr.get(i+1).get(j+1) + arr.get(i+2).get(j) + arr.get(i+2).get(j+1) + arr.get(i+2).get(j+2);
+            ans = Math.max(ans,sum);
+            }
+        }
+        return ans;
+
     }
 
 }
