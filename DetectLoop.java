0b655ad0bac1c57709830835cc89101bdcd14df2
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-05-09 20:08:56 +0530

    Program to detect a loop in a linked list

---
+++ DetectLoop.java
@@ -0,0 +1,13 @@
+public class DetectLoop {
+    static boolean hasCycle(SinglyLinkedListNode head) {
+        
+        SinglyLinkedListNode slow = head;
+        SinglyLinkedListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast) return true;
+        }
+            return false;
+    }
+}
